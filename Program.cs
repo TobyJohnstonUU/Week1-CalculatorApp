c680235418defa896d7b243b6fdd0392257a398e
Toby Johnston <TobyJohnstonUU@users.noreply.github.com>
2025-09-28 23:11:07 +0100

    Updated code to add more options

--- Program.cs
+++ Program.cs
@@ -1,7 +1,49 @@
-﻿Console.WriteLine("Type the first number, press Enter to confirm");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Type the second number, press Enter to confirm");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
-int result = firstNumber + secondNumber;
-Console.WriteLine("The addition of {0} and {1} is {2}", firstNumber, secondNumber, result);
-Console.ReadKey();
+﻿CalculatorApp();
+
+void CalculatorApp()
+{
+    int firstNumber = 0;
+    int secondNumber = 0;
+    int result = 0;
+    int choice = 0;
+
+    Console.WriteLine("Type the first number, press Enter to confirm");
+    firstNumber = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Type the second number, press Enter to confirm");
+    secondNumber = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Choose an option from the following list: ");
+    Console.WriteLine("1 - Add");
+    Console.WriteLine("2 - Subtract");
+    Console.WriteLine("3 - Divide");
+    Console.WriteLine("4 - Multiply");
+
+    choice = Convert.ToInt32(Console.ReadLine());
+
+    if (choice == 1)
+    {
+        result = firstNumber + secondNumber;
+        Console.WriteLine($"Adding {firstNumber} to {secondNumber} equals {result}");
+    }
+    else if (choice == 2)
+    {
+        result = firstNumber - secondNumber;
+        Console.WriteLine($"Subtracting {firstNumber} to {secondNumber} equals {result}");
+    }
+    else if (choice == 3)
+    {
+        result = firstNumber / secondNumber;
+        Console.WriteLine($"Dividing {firstNumber} to {secondNumber} equals {result}");
+    }
+    else if (choice == 4)
+    {
+        result = firstNumber * secondNumber;
+        Console.WriteLine($"Multiplying {firstNumber} to {secondNumber} equals {result}");
+    }
+    else
+    {
+        Console.WriteLine("You did not select a valid number between 1-4");
+    }
+
+    Console.ReadKey();
+}
