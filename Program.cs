485349f90ab2572b23301ae3482739aeb56f51b8
Toby Johnston <TobyJohnstonUU@users.noreply.github.com>
2025-09-28 22:32:02 +0100

    Implemented calculator program code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Type the first number, press Enter to confirm");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Type the second number, press Enter to confirm");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+int result = firstNumber + secondNumber;
+Console.WriteLine("The addition of {0} and {1} is {2}", firstNumber, secondNumber, result);
+Console.ReadKey();
